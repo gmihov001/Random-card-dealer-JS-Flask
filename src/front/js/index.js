8f4c6818b2043806b22c7d35693f056e646d64a9
George Mihov <gmihov001@gmail.com>
2020-06-06 00:33:03 +0000

    Updates

--- src/front/js/index.js
+++ src/front/js/index.js
@@ -5,12 +5,14 @@ import "../assets/img/4geeks.ico";
 //import 'breathecode-dom'; //DOM override to make JS easier to use
 import "../style/index.scss";
 
-window.onload = function() {
-  <div class="wrapper">
-    <div class="card">
-      <div class="top">{}</div>
-      <div class="middle"></div>
-      <div class="bottom"></div>
-    </div>
-  </div>;
+window.onload = async function() {
+  const response = await fetch(
+    "https://3000-dcd4c2c4-3ab1-4581-9793-454279c4b775.ws-us02.gitpod.io/api/card"
+  );
+  let response_body = await response.json();
+  console.log(response_body);
+  let suit_symbol = document.querySelector(".top div");
+  console.log(suit_symbol);
+
+  suit_symbol.innerHTML = "HEART";
 };
