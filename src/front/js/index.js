3e95a5232231e5043754c3fba44c14c248c0177c
George Mihov <gmihov001@gmail.com>
2020-06-06 00:54:23 +0000

    Updates

--- src/front/js/index.js
+++ src/front/js/index.js
@@ -10,9 +10,7 @@ window.onload = async function() {
     "https://3000-dcd4c2c4-3ab1-4581-9793-454279c4b775.ws-us02.gitpod.io/api/card"
   );
   let response_body = await response.json();
-  console.log(response_body);
-  let suit_symbol = document.querySelector(".top div");
-  console.log(suit_symbol);
 
-  suit_symbol.innerHTML = "HEART";
+  let top_symbol = document.querySelector(".top div");
+  top_symbol.innerHTML = response_body.suit;
 };
